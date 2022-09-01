b32c3aadc792c021c4be3678bd4ce75c1df99320
burhan-work <burhan.bnw@gmail.com>
2022-09-01 08:32:18 +0800

    update menu dan button

---
+++ boer/src/data.js
@@ -0,0 +1,20 @@
+import {SiOpenaigym} from 'react-icons/si';
+
+export const links = [
+    {
+        name: "Home",
+        path: '/'
+    },
+    {
+        name: "About",
+        path: '/about'
+    },
+    {
+        name: "Contact",
+        path: '/contact'
+    },
+    {
+        name: "Gallery",
+        path: '/gallery'
+    }
+]
